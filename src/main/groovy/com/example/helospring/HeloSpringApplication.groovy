0c288bab09266006ce27b3b39191e8ca5405f624
Andres Natera <andres.natera@viewnext.com>
2021-07-28 09:01:17 +0200

    este es primer commit InetlliJ

---
+++ src/main/groovy/com/example/helospring/HeloSpringApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.helospring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HeloSpringApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HeloSpringApplication, args)
+    }
+
+}
