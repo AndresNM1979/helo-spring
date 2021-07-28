e4b0813502443b9140489c8966482a3e5af52a2b
Andres Natera <andres.natera@viewnext.com>
2021-07-28 12:40:43 +0200

    Añadimos mapping a /hello action

--- src/main/groovy/com/example/helospring/HeloSpringApplication.groovy
+++ src/main/groovy/com/example/helospring/HeloSpringApplication.groovy
@@ -1,13 +1,21 @@
-package com.example.helospring
+package com.example.demo;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-class HeloSpringApplication {
+@RestController
+public class HeloSpringApplication {
 
-    static void main(String[] args) {
-        SpringApplication.run(HeloSpringApplication, args)
+    public static void main(String[] args) {
+        SpringApplication.run(HeloSpringApplication .class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
